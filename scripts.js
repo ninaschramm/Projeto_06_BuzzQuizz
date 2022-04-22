5c1d52c207106c0658f5c4a724e14e0f1bfa9dc1
thaisealves <alves.thaisesilva@gmail.com>
2022-04-22 18:44:39 -0300

    Finalizada validação de perguntas

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
-function getQuizzes() {
- const promise = axios.get("https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes")
+let title;
+let level;
+let questionQtt;
+let imgURL;
+let number;
 
- promise.then(listQuizzes)
- promise.catch(treatError)
+let id = 0;
+
+function getQuizzes() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes")
+
+    promise.then(listQuizzes)
+    promise.catch(treatError)
 }
 
 function treatError() {
@@ -17,28 +25,26 @@ const allQuizzes = document.getElementById("listAllQuizzes")
 function listQuizzes(quizzes) {
     funciona()
 
- for (let i=0; i<quizzes.data.length; i++) {
-     allQuizzes.innerHTML +=  `<li class="li_quizz" style="background-image: url('${quizzes.data[i].image}')" data-quiz="${quizzes.data[i].id}" onclick="initQuiz(this)">
+    for (let i = 0; i < quizzes.data.length; i++) {
+        allQuizzes.innerHTML += `<li class="li_quizz" style="background-image: url('${quizzes.data[i].image}')" data-quiz="${quizzes.data[i].id}" onclick="initQuiz(this)">
      <div>${quizzes.data[i].title}</div> 
  </li>`
- } 
+    }
 
-    
- if (quizzes.data.length < 3) {adjustSize()}
+
+    if (quizzes.data.length < 3) { adjustSize() }
 }
 
 getQuizzes()
 
 function adjustSize() {
-    {   document.querySelector(".li_quizz").classList.add("adjustMargin");
-        allQuizzes.classList.add("adjustSpace");}
+    {
+        document.querySelector(".li_quizz").classList.add("adjustMargin");
+        allQuizzes.classList.add("adjustSpace");
+    }
 }
 
-let title;
-let level;
-let questionQtt;
-let imgURL;
-let number;
+
 function createQuiz() {
     document.querySelector(".content.firstScreen").classList.add("hidden")
     document.querySelector(".content.thirdScreen").classList.remove("hidden")
@@ -70,32 +76,12 @@ function createQuestions() {
     ulQuestions.innerHTML = ""
     for (let i = 0; i < questionQtt; i++) {
         ulQuestions.innerHTML +=
-            `<li >
+            `<li class="questioning closed">
              <span>Pergunta ${i + 1}</span>
              <ion-icon onclick="openQuestion(this)" name="create-outline"></ion-icon>
-        </li>`
-    }
-}
 
-function openQuestion(elem) {
-    const opened = document.querySelector(".questioning")
-    if (opened !== null) {
-        opened.querySelector("div").classList.add("hidden")
-        opened.classList.remove("questioning")
-        opened.innerHTML += `<span>Pergunta ${number + 1}</span>
-        <ion-icon onclick="openQuestion(this)" name="create-outline"></ion-icon>` //keeps the number of the last question, because it's before the next definition 
-    }
-
-    // number = Array.from(elem.closest("li").parentNode.children).indexOf(elem.closest("li")) //searching from the number of the question I'm clicking on ** creating an array so I can take the index
-     number = Array.from((elem.parentNode).parentNode.children).indexOf(elem.parentElement)
-     console.log(number)
-    
-    elem.closest("li").classList.add("questioning");
-    elem.closest("li").innerHTML +=
-
-        `<div>
+             <div class="hidden">
         <div>
-            <span>Pergunta ${number + 1}</span>
             <input class="question" placeholder="Texto da pergunta" type="text">
             <input class="question-color" placeholder="Cor de fundo da pergunta" type="text">
         </div>
@@ -118,7 +104,24 @@ function openQuestion(elem) {
             <input placeholder="URL da imagem 3" type="text">
         </div>
         </div>
-        `
+        </li>`
+    }
+}
+
+function openQuestion(elem) {
+    const open = (elem.parentElement).querySelector("div")
+    const opened = (elem.parentElement).parentElement.querySelector(".opened")
+    if (opened !== null) {
+        opened.classList.remove("opened")
+        opened.classList.add("closed")
+        opened.querySelector("div").classList.add("hidden")
+    }
+    elem.parentElement.classList.remove("closed")
+    open.classList.remove("hidden")
+    elem.parentElement.classList.add("opened")
+    console.log(elem)
+
+
 }
 
 //BIG PROBLEM, WHEN NONE IS OPEN, NONE HAS "QUESTIONING", THINK OF ANOTHER WAY OF MAKING IT WORK! I HAVE TO HAVE THE INPUTS VALUES EVEN IF ITS CLOSED! 
@@ -129,30 +132,40 @@ function questionsValidation() {
     imgURL = document.querySelectorAll(".imgURL")
     const re = /[0-9A-Fa-f]{6}/g;
 
-    console.log(question.length)
+
     for (let i = 0; i < question.length; i++) {
         if (question[i].value.length < 20) {
+            console.log("a")
             return false;
         }
     }
 
     for (let i = 0; i < answer.length; i++) {
         if (answer[i].value === "") {
+            console.log("b")
+            return false
+        }
+    }
+
+    for (let i = 0; i < imgURL.length; i++) {
+        if (!(imgURL[i].value.startsWith('https'))) {
+            console.log("d")
             return false
         }
     }
 
     for (let i = 0; i < questionColor.length; i++) {
-
-        if (!(re.test(questionColor[i].value))) {
+        let reTest = re.test(questionColor[i].value)
+        console.log(questionColor[i].value)
+        console.log(reTest)
+        if (reTest !== true) {
+            console.log("c")
+            console.log(reTest)
             return false;
         }
+        re.lastIndex = 0;
     }
-    for (let i = 0; i < imgURL.length; i++) {
-        if (!(imgURL[i].value.startsWith('https'))) {
-            return false
-        }
-    }
+    console.log("e")
     return true;
 }
 
@@ -168,8 +181,6 @@ function createLevels() {
 
 }
 
-let id = 0;
-
 function initQuiz(idQuiz) {
     id = idQuiz.dataset.quiz;
     document.querySelector(".firstScreen").classList.add("hidden")
@@ -181,23 +192,23 @@ function initQuiz(idQuiz) {
 function takeQuiz(resposta) {
     console.log(id)
     document.querySelector(".headQuizz").innerHTML = `<img src="${resposta.data.image}" /> <div class="titleQuiz">${resposta.data.title}</div> <div class="hideHead"></div>`
-    for (let i=0; i<resposta.data.questions.length; i++) { 
+    for (let i = 0; i < resposta.data.questions.length; i++) {
         const answersArr = []
-     for (let j=0; j<resposta.data.questions[i].answers.length; j++) {
-         answersArr.push(resposta.data.questions[i].answers[j])
-         answersArr.sort(comparador)
-     }  
-    document.querySelector(".questionsQuizz").innerHTML += `<div class="question">
+        for (let j = 0; j < resposta.data.questions[i].answers.length; j++) {
+            answersArr.push(resposta.data.questions[i].answers[j])
+            answersArr.sort(comparador)
+        }
+        document.querySelector(".questionsQuizz").innerHTML += `<div class="question">
     <div class="questionTitle" style="background-color:${resposta.data.questions[i].color}">${resposta.data.questions[i].title}</div>
    <div class="answers n_${[i]}"></div>
 </div>`
-for (let y=0; y< answersArr.length; y++) {
-    document.querySelector(`.answers.n_${[i]}`).innerHTML += `<div class="answer" onclick="chooseAnswer(this)"><img src="${answersArr[y].image}" /> ${answersArr[y].text}`
-}
+        for (let y = 0; y < answersArr.length; y++) {
+            document.querySelector(`.answers.n_${[i]}`).innerHTML += `<div class="answer" onclick="chooseAnswer(this)"><img src="${answersArr[y].image}" /> ${answersArr[y].text}`
+        }
     }
 
 }
 
-function comparador() { 
-	return Math.random() - 0.5; 
+function comparador() {
+    return Math.random() - 0.5;
 }
